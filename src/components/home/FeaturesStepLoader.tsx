1fa3520fc9b8189dbd60c67d9dd2e28f1a10a526
Ziad Ali <ziaddalii11@gmail.com>
2024-05-30 06:31:19 +0300

    first commit

---
+++ src/components/home/FeaturesStepLoader.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import { MultiStepLoader as Loader } from "../ui/multi-step-loader";
+import { IconSquareRoundedX } from "@tabler/icons-react";
+
+const loadingStates = [
+  {
+    text: "Buying a condo",
+  },
+  {
+    text: "Travelling in a flight",
+  },
+  {
+    text: "Meeting Tyler Durden",
+  },
+  {
+    text: "He makes soap",
+  },
+  {
+    text: "We goto a bar",
+  },
+  {
+    text: "Start a fight",
+  },
+  {
+    text: "We like it",
+  },
+  {
+    text: "Welcome to F**** C***",
+  },
+];
+
+export function MultiStepLoaderDemo() {
+  return (
+    <div className="w-full absolute left-0 top-1/2">
+      <Loader loadingStates={loadingStates} loading={true} duration={2000} />
+    </div>
+  );
+}
