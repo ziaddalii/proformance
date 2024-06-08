863063e101fa4a58fdd2c8e4ed81f2fc552a6bfa
Ziad Ali <ziaddalii11@gmail.com>
2024-06-08 11:28:54 +0300

    fix build issues

--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -25,7 +25,7 @@ export const HoverEffect = ({
     >
       {items.map((item, idx) => (
         <div
-          key={item?.link}
+          key={idx}
           className="relative group  block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
